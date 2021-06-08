dfd0e4b547f38aa00edf6aadc67b4de7861298b8
okihusni <okihusni@gmail.com>
2021-06-08 09:45:31 +0700

    Setup deployment to heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require('cors');
 const routes = require('./routes');
 const errorHandlers = require('./middlewares/errorhandlers');
