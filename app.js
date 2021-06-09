38fd5e58fd192a276f60e97255372d4522f6ac87
VenansiusRendy <lierendysetiawan3232@gmail.com>
2021-06-09 20:23:23 +0800

    add env port in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const router = require("./router");
 
 app.use(cors());
